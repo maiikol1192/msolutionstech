22a73267d8ed4993329c6da3d315eccead753c5c
Michael Rodriguez <maiikol1192@gmail.com>
2021-07-22 08:52:35 -0500

    Cambios

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const app = new Vue({
         },
 
         link(){
-            return "https://wa.me/573144049124?text=Hola M SOLUTIONS TECH mi nombre es *" + this.nombre + "* , estoy interesado en saber sobre *" + this.opcion + "* para que me puedas contactar mi numero es: *" + this.telefono + this.numero + "*."
+            return "https://wa.me/573144049124?text=Hola M SOLUTIONS TECH mi nombre es *" + this.nombre + "* , estoy interesado en saber sobre *" + this.opcion + "* para que me puedas contactar mi numero es: *" + this.telefono + "*."
         }
 
     }
